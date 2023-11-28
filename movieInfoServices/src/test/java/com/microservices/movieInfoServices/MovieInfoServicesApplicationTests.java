7633dd22555ef4a4e3ce0ea36503b9800ebf2aec
Aniruddhaphatangare <107043737+Aniruddhaphatangare@users.noreply.github.com>
2023-11-28 07:09:35 +0530

    Add files via upload
    Initial Commit

---
+++ movieInfoServices/src/test/java/com/microservices/movieInfoServices/MovieInfoServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.movieInfoServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
